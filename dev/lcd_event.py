0a750a49a91bb6f7946772ffe2617285e5c2c774
qkzk <qu3nt1n@gmail.com>
2016-12-27 00:04:38 +0100

    separation du display, creation amelioration lcd event

--- dev/lcd_event.py
+++ dev/lcd_event.py
@@ -22,21 +22,38 @@ def conversion_horaire(str):
     return str_mktime
 
 # l'evenement qui sera execute a l'heure dite
-def event_response():
-    print("et c'est parti ! ",time.time())
+def event_response(type_event):
+    if type_event == 0:
+        print("event 0 ",time.time())
+    elif type_event == 1:
+        print("event 1 ",time.time())
+    elif type_event == 2:
+        print("event 2 ",time.time())
+
 
 # l'attente et la reponse (il sleep() entre les deux)
 # si le time est dans le passé il s'exécute immediatement
-def waittilltime(str):
+def waittilltime(str,type_event):
     print("on attend a partir de : ",time.time())
     lancement = conversion_horaire(str)
-    s.enterabs(lancement, 1, event_response, ())
+    s.enterabs(lancement, 1, event_response, [type_event])
     s.run()
     print(time.time())
     print("fini")
 
+'''
+le principe est :
+    * se lancer à 0h00 tous les jours
+    * s'il y a cours : récupérer l'horaire du train et lancer le scheduler
+    * sinon passer et quitter (cron fera le lendemain)
+    * scheduler : attend jusque x min avant le train et affiche msg sur lcd
+    * lance le second scheduler etc.
+'''
+
 
 if __name__ == '__main__':
     # ici on a récupéré une date au format '20161226T13381800'
-    horaire = "20161226T19340000"
-    waittilltime(horaire)
+    horaires = ["20161226T23420000","20161226T23430000","20161226T23440000"]
+    waittilltime(horaires[0],0)
+    waittilltime(horaires[1],1)
+    waittilltime(horaires[2],2)
