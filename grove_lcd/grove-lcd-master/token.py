a27481271a6c29ab999c8481caf1a8bbd7d33577
qkzk <qu3nt1n@gmail.com>
2017-01-18 20:40:38 +0100

    Delete token.py

--- grove_lcd/grove-lcd-master/token.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-sandboxToken = '57c898d2-84b4-42f6-bbc8-a05263defbda'
-
-# exemple d'url pour tester
-
-# url = "https://57c898d2-84b4-42f6-bbc8-a05263defbda@api.sncf.com/v1/coverage/sncf/journeys?from=stop_area:OCE:SA:87286005&to=stop_area:OCE:SA:87286302&datetime_represents=arrival&datetime=20161229T080000&"
